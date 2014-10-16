c837d3631a5208885c7b40cac2e64ccb5e2673e4
Piotr <kedzpipl@gmail.com>
2014-10-16 13:14:02 +0200

    Ready to deploy Version 1.0

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,6 @@
+class ApplicationController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  add_flash_types(:danger)
+  protect_from_forgery with: :exception
+end
