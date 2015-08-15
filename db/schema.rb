c6a5b880cdfe77ef3d1f58a40bec5d0710b4b2fd
Piotr Kędzia <piotr@kedzia.eu>
2015-08-15 14:03:37 +0200

    Change associations in User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150815101507) do
+ActiveRecord::Schema.define(version: 20150815114857) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "text"
+    t.integer  "favorite_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "comments", ["favorite_id"], name: "index_comments_on_favorite_id", using: :btree
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "favorites", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "comment_id"
+  end
+
+  add_index "favorites", ["comment_id"], name: "index_favorites_on_comment_id", using: :btree
+  add_index "favorites", ["user_id"], name: "index_favorites_on_user_id", using: :btree
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
@@ -63,4 +86,8 @@ ActiveRecord::Schema.define(version: 20150815101507) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "favorites"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "comments"
+  add_foreign_key "favorites", "users"
 end
